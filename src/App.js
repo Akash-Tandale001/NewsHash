f3f4c8be9de74edb5005f8862a1f98fbc6d0b93e
Akash-Tandale001 <ak.tandale123@gmail.com>
2022-01-02 15:44:11 +0530

    Initial Structure of website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import {BrowserRouter as Router , Route , Routes } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Newsfetch from './components/Newsfetch';
+import Home from './pages/Home';
 
-function App() {
+
+function App()  {
+  // const apiKey = process.env.REACT_APP_NEWS_API;
   return (
-  <>
-  <div>
-    NewsHash
-  </div>
-  </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />        
+        <Route path="/general" element={<Newsfetch />} />
+        <Route path="/business" element={<Newsfetch />} />
+        <Route path="/entertainment" element={<Newsfetch />} />
+        <Route path="/technology" element={<Newsfetch />} />
+        <Route path="/sports" element={<Newsfetch />} />
+        <Route path="/science" element={<Newsfetch />} />
+        <Route path="/health" element={<Newsfetch />} />        
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Newscard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import noimage from '../images/notImage.jpg'
+
+const Newscard = (props) => {
+    let {title ,description,imageurl,newsurl,author,date,source} = props;
+    return (
+        <div className='my-3'>
+        <div className="card shadow p-3 mb-5 bg-body rounded" style={{width:"18rem"}}>
+        <span class="badge rounded-pill bg-info text-dark">{source}</span>
+            <img src={!imageurl ? noimage : imageurl } className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title} </h5>
+                    <p className="card-text">{description} </p>
+                    <a href={newsurl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary d-flex justify-content-center">Read more</a>
+                    <p class="card-text"><small className="text-muted">By {!author? "Unknown":author} on {new Date(date).toGMTString()}</small></p>
+                </div>
+        </div>
+    </div>
+    )
+}
+
+export default Newscard
